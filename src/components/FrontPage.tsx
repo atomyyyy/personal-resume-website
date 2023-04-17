6a55853caa4b9f1e8b96791322d11158630197ff
Atomy <atomyuen95@gmail.com>
2023-04-17 17:12:41 +0000

    release: go live for first version

--- src/components/FrontPage.tsx
+++ src/components/FrontPage.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
+// import Button from '@mui/material/Button';
 
 import GithubIcon from './Icon/GithubIcon';
 import LinkedinIcon from './Icon/LinkedinIcon';
